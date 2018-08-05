a43e3526c647e0d7b792bd40da2903cf03a1acae
Sanda <sanda.golcea@gmail.com>
2018-08-05 21:55:28 +0100

    react-router: index route with Switch play, and Redirect-ion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 
-import { BrowserRouter as Router, Link , Route } from 'react-router-dom';
+import { BrowserRouter as Router, Link , Route, Switch } from 'react-router-dom';
 
 // components
 import Home from './components/Home';
@@ -11,13 +11,15 @@ import Dropdown from './components/common/Dropdown';
 import './App.css';
 
 class App extends Component {
+
   render() {
+    const text="rarar";
     return (
       <div className="App">
 
-        <Router>
+        <Router basename="/">
           <div>
-            <Route exact={true} path="/" render={() => (
+            <Route path="/" render={() => (
                 <header className="App-header">
                   <img src={logo} className="App-logo" alt="logo" />
                   <h1 className="App-title">Welcome to React</h1>
@@ -29,13 +31,11 @@ class App extends Component {
               <li><Link to={'/products-page/123'}>Products</Link></li>
             </ol>
 
-            <Route path="/home" component={Home} />
-            <Route path="/products-page/:productId"
-              render={({match}) => (
-                <ProductsPage text={"example prop"} match={match}/>
-              )}
-            />
-
+            <Route exact path="/home" component={Home} />
+            <Switch>
+              <Route exact path="/products-page" render={() => (<ProductsPage text={text} /> )} />
+              <Route path="/products-page/:productId" component={ProductsPage} />
+            </Switch>
           </div>
         </Router>
       </div>
--- src/components/ProductsPage.js
+++ src/components/ProductsPage.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import {Redirect} from 'react-router';
 import { Link } from 'react-router-dom';
 
 const ProductsPage = ({ match, text }) => (
     <div>
+    	{match && match.params.productId != 123 && <Redirect to={{pathname: '/products-page'}}/>}
       <h3>Sample products page</h3>
 			<span>Go to: </span>
 			<Link to='/home'>
 				Go to the home page
 			</Link>
-			<div>Product id: {match.params.productId}</div>
-			<div>Extra parameter text: {text}</div>
+			{!match && <h5>Vanilla product page</h5>}
+			{match && <div>Product id: {match.params.productId}</div>}
+			{text && <div>Extra parameter text: {text}</div>}
     </div>
 )
 
